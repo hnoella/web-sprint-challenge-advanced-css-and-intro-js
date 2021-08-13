50b83cff80c9e06843066d84b0858f69a6f6344f
Noella Hirwa <noellahirwa@Noellas-MacBook-Pro.local>
2021-08-13 13:26:02 -0500

    using reponsive design to creat a mobile friendly page and finised tasks on index.js

--- index.js
+++ index.js
@@ -238,14 +238,13 @@ Example born in 1901 and died in 1959 - included -- born in 1889 and died in 192
 If correct, the function should return ["Salvador Dali", "Frida Kahlo"]*/
 // Hint - Look up the .split() method
 
-function get20s(array) {
-  const year20 = array;
-}
+function get20s(array) {}
+console.log("task 4", get20s(artists));
 
 // function filterByWord(array, string) {
 //   const filteredArray = [];
 //   for (let i = 0; i < array.length; i++) {
-//     if (array[i].includes(string)) {
+//     if (array[i].includes(years)) {
 //       filteredArray.push(array[i]);
 //     }
 //   }
@@ -262,7 +261,12 @@ Use removeArtist to do the following:
 For example, if removeArtist is invoked with the artists array and the number 0, it will remove Amedeo Modigliani from our dataset and return the number 19. */
 
 function removeArtist(array, index) {
-  return array[index].length;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === [index]) {
+      array.splice(i, 1);
+    }
+  }
+  return array.length;
 }
 console.log("task 5", removeArtist(artists, 7));
 
